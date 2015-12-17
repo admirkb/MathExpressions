2ba8c7c6d2c94aad93d6bf5e93ab3cc5f7e250b6
kelvinbeech <kelvin.beech@kjb.co.uk>
2015-12-17 16:12:25 +0000

    Addes some unit tests

---
+++ MathExpressionsTest/MathTest1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using KBMath;
+
+namespace MathExpressionsTest
+{
+    [TestClass]
+    public class MathTest1
+    {
+        [TestMethod]
+        public void AddTwoNumbers()
+        {
+            // Arrange
+            var mathParser = new MathParser('.');
+            var input = "12 + 12";
+            var output = 24d;
+
+            // Act
+            var actual = mathParser.Parse(input);
+
+            // Assert
+            Assert.AreEqual(output, actual);
+        }
+
+        [TestMethod]
+        public void AddWithDivision()
+        {
+            // Arrange
+            var mathParser = new MathParser('.');
+            var input = "(12 + 12) / 3";
+            var output = 8d;
+
+            // Act
+            var actual = mathParser.Parse(input);
+
+            // Assert
+            Assert.AreEqual(output, actual);
+        }
+        [TestMethod]
+        public void AddWithMultiply()
+        {
+            // Arrange
+            var mathParser = new MathParser('.');
+            var input = "( 5 + 5 ) * (3 + 2 )";
+            var output = 50d;
+
+            // Act
+            var actual = mathParser.Parse(input);
+
+            // Assert
+            Assert.AreEqual(output, actual);
+        }
+
+    }
+}
